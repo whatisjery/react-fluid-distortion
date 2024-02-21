ac774e1d859f7042a73fd726d8078aa5b3875b73
Jérémie Nallet <jeremienallet@gmail.com>
2024-02-21 22:23:33 +0100

    refactor

--- glsl.d.ts
+++ glsl.d.ts
@@ -1,18 +1,13 @@
-// custom.d.ts or any other .d.ts file in your project
-
-// Declarations for .glsl
 declare module '*.glsl' {
     const value: string;
     export default value;
 }
 
-// Declarations for .vert
 declare module '*.vert' {
     const value: string;
     export default value;
 }
 
-// Declarations for .frag
 declare module '*.frag' {
     const value: string;
     export default value;
